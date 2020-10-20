37248fa8b8f6a8ade0129ae90645aad737a82eef
luigi manga <luigi.manga92@gmail.com>
2020-10-20 18:12:16 +0200

    lc

---
+++ db/migrate/20201020153727_create_pets.rb
@@ -0,0 +1,10 @@
+class CreatePets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
